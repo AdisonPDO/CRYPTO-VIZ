072010beb259e8c03bd24ad24231a72618585da8
AdisonP <52446207+AdisonPDO@users.noreply.github.com>
2023-10-19 21:37:34 +0200

    Update script.py

--- spark/script.py
+++ spark/script.py
@@ -1,20 +1,32 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
+# Binance Spark Streaming
+
 # Création d'une session Spark
 spark = SparkSession.builder.appName("KafkaSparkStreaming").getOrCreate()
 
 # Config de la session Spark
 kafka_stream = spark.readStream.format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:9092") \
-    .option("subscribe", "nom_du_topic") \
+    .option("subscribe", "datas_binance") \
     .load()
 
 # Traitement des données
-processed_data = kafka_stream.selectExpr("CAST(value AS STRING)")
+
+# Definition du schema pour le type de message suivant
+# let crypto = {
+#     "name": stockObject.s,
+#     "date": new Date(stockObject.E).toISOString(),
+#     "value": parseFloat(stockObject.p).toFixed(2)
+# }
+schema = StructType().add("name", StringTypes()).add("date", StringType()).add("value", StringType())
+
+# Conversion des données en format df
+data_df = kafka_stream.selectExpr("CAST(value AS STRING)").select(from_json("value", schema).alias("crypto_data"))
 
 # Sortie des données
-query = processed_data.writeStream.outputMode("append") \
+query = data_df.writeStream.outputMode("append") \
     .format("console") \
     .start()
 
