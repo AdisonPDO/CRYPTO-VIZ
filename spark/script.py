92d367cf7e84f2d08c83ae8cb35c2306d9400c39
AdisonP <52446207+AdisonPDO@users.noreply.github.com>
2024-01-11 15:19:12 +0100

    spark ok

--- spark/script.py
+++ spark/script.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+from pyspark.sql.types import StructType, StructField, StringType
 
 # Binance Spark Streaming
 
@@ -8,7 +9,7 @@ spark = SparkSession.builder.appName("KafkaSparkStreaming").getOrCreate()
 
 # Config de la session Spark
 kafka_stream = spark.readStream.format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("kafka.bootstrap.servers", "kafka:9092") \
     .option("subscribe", "datas_binance") \
     .load()
 
@@ -20,14 +21,24 @@ kafka_stream = spark.readStream.format("kafka") \
 #     "date": new Date(stockObject.E).toISOString(),
 #     "value": parseFloat(stockObject.p).toFixed(2)
 # }
-schema = StructType().add("name", StringType()).add("date", StringType()).add("value", StringType())
+# {"crypto":{"name":"BTCUSDT","date":"2024-01-11T13:55:14.993Z","value":"47316.66"}}
 
+schema = StructType([
+    StructField("crypto", StructType([
+        StructField("name", StringType()),
+        StructField("date", StringType()),
+        StructField("value", StringType())
+    ]))
+])
 # Conversion des données en format df
-data_df = kafka_stream.selectExpr("CAST(value AS STRING)").select(from_json("value", schema).alias("crypto_data"))
-
+#data_df = kafka_stream.selectExpr("CAST(value AS STRING)").select(from_json("value", schema).alias("crypto_data"))
+data_df = kafka_stream.selectExpr("CAST(value AS STRING)") \
+                      .select(from_json("value", schema).alias("data")) \
+                      .select("data.crypto.*")
 # Sortie des données
 query = data_df.writeStream.outputMode("append") \
     .format("console") \
     .start()
 
 query.awaitTermination()
+
